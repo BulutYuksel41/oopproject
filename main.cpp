1b3dcd2de843bded60ab182a5b1f3ca5dcd29f4f
Bulut Yüksel <23010903088@subu.edu.tr>
2025-01-05 15:50:58 +0300

    updating the project

--- main.cpp
+++ main.cpp
@@ -21,4 +21,40 @@ private:
     void GenerateFruit() {
         fruit.first = rand() % width;
         fruit.second = rand() % height;
-    }}
+    }
+    void Draw() {
+        COORD coord = {0, 0};
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+        for (int i = 0; i < width + 2; i++)
+            cout << "#";
+        cout << endl;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (j == 0) cout << "#";
+                if (make_pair(j, i) == snake[0])
+                    cout << "O";
+                else if (make_pair(j, i) == fruit)
+                    cout << "F";
+                else {
+                    bool isTail = false;
+                    for (int k = 1; k < snake.size(); k++) {
+                        if (snake[k] == make_pair(j, i)) {
+                            cout << "o";
+                            isTail = true;
+                            break;
+                        }
+                    }
+                    if (!isTail)
+                        cout << " ";
+                }
+                if (j == width - 1) cout << "#";
+            }
+            cout << endl;
+        }
+
+        for (int i = 0; i < width + 2; i++)
+            cout << "#";
+        cout << endl;
+        cout << "Score: " << score << endl;
+    }};
